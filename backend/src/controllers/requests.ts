2316fe70bc7051042567a1c23989303cfff75a95
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-06-10 21:11:38 +0600

    ex-13 [backend] Внесла правки в роуте на создание заявки

--- backend/src/controllers/requests.ts
+++ backend/src/controllers/requests.ts
@@ -1,22 +1,29 @@
+import { Request, Response } from "express";
 import RequestFromClient from "../models/Request";
-import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
 
-export const getRequests = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {name, phoneNumber} = req.body;
-        if (!name || !phoneNumber || !name.trim()  || !phoneNumber.trim()) {
-            res.status(400).send({error: "Поля имя и номер обязательны"});
-            return;
+export const postRequest =  async (req: Request, res: Response) => {
+        try {
+            const {name, phone, email} = req.body;
+            if (!name.trim()  || !phone.trim() || !email.trim()) {
+                res.status(400).send({error: "Поля имя, номер, почта обязательны"});
+                return;
+            }
+
+            const request = new RequestFromClient({
+                name,
+                phone,
+                email
+            });
+
+            await request.save();
+            res.send({message: "Заявка отправлена"});
+        } catch (e) {
+            if (e instanceof mongoose.Error.ValidationError) {
+                const error = Object.values(e.errors)[0]?.message;
+                res.status(400).send({ message: error });
+                return
+            }
+            res.status(500).send({ message: "Что-то пошло не так" });
         }
-
-        const request = new RequestFromClient({
-            name,
-            phoneNumber
-        });
-
-        await request.save();
-        res.send({message: "Заявка отправлена"});
-    } catch (e) {
-        next(e);
-    }
-}
+};
