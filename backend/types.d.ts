b7047046aa6ac62fc5b085c28a79edabbc5d335f
nurlybek <orozovnurlybek@gmail.com>
2025-06-11 14:30:07 +0600

    [Backend] types.d.ts fixed

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,56 +1,31 @@
-export interface PortfolioItemPreview {
+export interface Product {
     _id: string;
-    description: string;
-    cover: string;
+    category: string;
+    title: string;
+    description: string | null;
+    image: string | null;
 }
 
-export interface GalleryItem {
-    _id: string;
-    image: string;
-}
-
-export interface PortfolioItemDetail extends PortfolioItemPreview {
-    gallery: GalleryItem[];
-}
+export type ProductWithoutId = Omit<Product, '_id'>;
 
 export interface Category {
     _id: string;
     title: string;
 }
 
-export interface Product {
-    _id: string;
-    title: string;
-    category: {
-        _id: string;
-        title: string;
-    };
-    image: string
-    description: string | null;
-}
-
-export interface ValidationError {
-    errors: {
-        [key: string]: {
-            name: string;
-            message: string;
-        };
-    };
-    message: string;
-    name: string;
-    _message: string;
-}
-
-export interface GlobalMessage {
-    error: string;
-}
-export interface IRequestMutation {
-    name: string;
+export interface UserFields {
     email: string;
-    phone: string;
+    password: string;
+    refreshToken: string;
+    role: "admin" | "superadmin";
+    __confirmPassword: string;
+    displayName: string;
 }
 
-export interface ContactDataDTO {
-    _id: string;
-
+export interface UpdatesRequest {
+    name?: string;
+    email?: string;
+    phone?: string;
+    status?: 'Новая'| 'В работе' | 'Завершена'| 'Отклонена';
+    commentOfManager?: string | null;
 }
