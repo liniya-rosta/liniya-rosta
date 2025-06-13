6db5e0641a4c5c35996ec6dd094294513d3dd230
nurlybek <orozovnurlybek@gmail.com>
2025-06-13 15:12:21 +0600

    [Fix] PR to dev

--- frontend/app/blog/[id]/page.tsx
+++ frontend/app/blog/[id]/page.tsx
@@ -5,6 +5,7 @@ type Params = { id: string };
 
 const PostPage = async ({ params }: { params: Promise<Params> }) => {
     const { id } = await params;
+
     return (
         <main className="container mx-auto px-4 py-8">
             <PostClient postId={id} />
