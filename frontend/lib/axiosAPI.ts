11fe590d20e3ac6627aff3a05014867b27c118fb
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-06-19 14:16:29 +0600

    ex-20 [frontend] Изменение хранения accessToken. Теперь токен хранится в local storage

--- frontend/lib/axiosAPI.ts
+++ frontend/lib/axiosAPI.ts
@@ -1,48 +1,46 @@
 import axios from "axios";
 import {API_BASE_URL} from "@/lib/globalConstants";
-import useAuthStore from "@/store/authStore";
 import {refreshAccessToken} from "@/actions/users";
 import useUserStore from "@/store/usersStore";
 
 const axiosAPI = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true,
+    baseURL: API_BASE_URL,
+    withCredentials: true,
 });
 
 axiosAPI.interceptors.request.use(config => {
-  const { accessToken } = useAuthStore.getState();
-  if (accessToken && config.headers) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
+    const {accessToken} = useUserStore.getState();
+    if (accessToken && config.headers) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
 });
 
 axiosAPI.interceptors.response.use(
     res => res,
     async (error) => {
-      const originalRequest = error.config;
+        const originalRequest = error.config;
 
-      if (error.response?.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
 
-        try {
-          const newToken = await refreshAccessToken();
+            try {
+                const newToken = await refreshAccessToken();
+                useUserStore.getState().setAccessToken(newToken);
 
-          useAuthStore.getState().setAccessToken(newToken);
+                if (originalRequest.headers) {
+                    originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                }
 
-          if (originalRequest.headers) {
-            originalRequest.headers.Authorization = `Bearer ${newToken}`;
-          }
-
-          return axiosAPI(originalRequest); // повтор запроса
-        } catch (e) {
-          useAuthStore.getState().setAccessToken(null);
-          useUserStore.getState().setLogout();
-          return Promise.reject(e);
+                return axiosAPI(originalRequest);
+            } catch (e) {
+                useUserStore.getState().setAccessToken(null);
+                useUserStore.getState().setLogout();
+                return Promise.reject(e);
+            }
         }
-      }
 
-      return Promise.reject(error);
+        return Promise.reject(error);
     }
 );
 
