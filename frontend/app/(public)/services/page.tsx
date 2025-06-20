f3a271af0a4476b33216826ba4db99969228faec
Kachkynov Alikhan <t8t228@gmail.com>
2025-06-20 21:03:56 +0600

    [ex-31] - загрузил фоновое изображение главное секции страницы услун

--- frontend/app/(public)/services/page.tsx
+++ frontend/app/(public)/services/page.tsx
@@ -7,7 +7,7 @@ export default function ServicePage() {
     return (
         <section className="min-h-screen bg-white">
             <div
-                className="w-full min-h-[560px] md:h-[560px] bg-black/50 bg-[url('/images/services/main-service.jpg')] bg-cover bg-center bg-blend-overlay mb-10">
+                className="w-full min-h-[560px] md:h-[560px] bg-black/50 bg-[url('/images/services/main-service.JPG')] bg-cover bg-center bg-blend-overlay mb-10">
                 <div
                     className="max-w-7xl mx-auto min-h-full px-6 py-15 md:py-0 flex flex-col md:flex-row items-center justify-between gap-10">
                     <ServicesTitle/>
