8ad30e7f6e6c34179efd72fb902a371b2441cce4
Kachkynov Alikhan <t8t228@gmail.com>
2025-08-07 23:21:06 +0600

    ex-98-translate-meta [frontend] - 1) передал в users.ts - логин, регистрацию, логаут, запрос по рефреш токену, редактироваие профиля на kyAPI вместо axiosAPI

--- frontend/actions/users.ts
+++ frontend/actions/users.ts
@@ -1,36 +1,37 @@
-import axiosAPI from "@/src/lib/axiosAPI";
 import {EditProfileForm, UserForm} from "@/src/lib/types";
+import kyAPI from "@/src/lib/kyAPI";
 
 export const login = async (data: UserForm) => {
     try {
-        const { email, password, confirmPassword } = data;
+        const {email, password, confirmPassword} = data;
 
-        const response = await axiosAPI.post("/users/sessions",
-            { email, password, confirmPassword });
-        return response.data;
+        return await kyAPI.post("users/sessions", {
+            json: {email, password, confirmPassword}
+        }).json();
     } catch (e) {
-       console.log(e);
+        console.log(e);
     }
 };
 
 export const refreshAccessToken = async () => {
     try {
-        const response = await axiosAPI.post("/users/refresh-token",);
-        return response.data.accessToken;
-    } catch(e) {
+        const res = await kyAPI.post("users/refresh-token").json<{ accessToken: string }>();
+        return res.accessToken;
+    } catch (e) {
         console.log(e);
     }
 };
 
 export const logout = async () => {
     try {
-        await axiosAPI.delete("/users/logout");
-    }catch(e) {
+        await kyAPI.delete("users/logout");
+    } catch (e) {
         console.log(e);
     }
 }
 
 export const editProfile = async (data: EditProfileForm) => {
-    const response = await axiosAPI.patch('/users/profile', data);
-    return response.data;
+    return await kyAPI.patch("users/profile", {
+        json: data
+    }).json();
 };
