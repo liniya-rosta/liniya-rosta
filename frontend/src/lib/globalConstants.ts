735f0e01c5e9d73084424191042ed08c15e986c2
Kachkynov Alikhan <t8t228@gmail.com>
2025-08-26 04:20:52 +0600

    изменил API_BASE_URL и IMG_BASE с проверкой серверных запросов на сервере

--- frontend/src/lib/globalConstants.ts
+++ frontend/src/lib/globalConstants.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:8000"
-export const IMG_BASE = "http://localhost:8000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? '/api';
+export const IMG_BASE = process.env.NEXT_PUBLIC_IMG_SERVER ?? '/';
 
 const wsBase = API_BASE_URL.replace(/^http/, "ws");
 export const WS_URL = `${wsBase}/ws/online-chat`;
