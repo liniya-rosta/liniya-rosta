6d6366a60ce684d09b39aef1ffec23a84ade3d8c
Kachkynov Alikhan <t8t228@gmail.com>
2025-08-27 13:00:33 +0600

    fix: инстаграм посты, заменил на https

--- frontend/src/app/(public)/[locale]/(home)/components/InstagramSection.tsx
+++ frontend/src/app/(public)/[locale]/(home)/components/InstagramSection.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from 'next-intl';
 import React from 'react';
 import SectionAnimation from "@/src/components/shared/SectionAnimation";
+import Script from "next/script";
 
 const InstagramSection = () => {
     const tHome = useTranslations("HomePage");
@@ -9,10 +10,21 @@ const InstagramSection = () => {
             <h3 className="main-section-title text-center text-23-30-1_5">
                 {tHome("instagramTitle")}
             </h3>
-            <iframe
-                src="//lightwidget.com/widgets/a5595befc0b75c39ae732dfc56693cbd.html"
-                className="lightwidget-widget w-full h-[380px] rounded-xl"
-            ></iframe>
+
+            <Script
+                src="https://cdn.lightwidget.com/widgets/lightwidget.js"
+                strategy="afterInteractive"
+            />
+
+            <div className="rounded-xl overflow-hidden">
+                <iframe
+                    src="https://cdn.lightwidget.com/widgets/a5595befc0b75c39ae732dfc56693cbd.html"
+                    className="lightwidget-widget w-full h-[380px]"
+                    scrolling="no"
+                    allowTransparency
+                />
+            </div>
+
         </SectionAnimation>
     );
 };
